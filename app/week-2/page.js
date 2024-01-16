de3853403de741b4dc0be6dca5009d09ed356845
cuiyiye <130084009+cuiyiye@users.noreply.github.com>
2024-01-16 10:37:44 -0700

    complete week-2 demo

---
+++ app/week-2/page.js
@@ -0,0 +1,15 @@
+import Heading from "./heading.js";
+
+export default function Page() {
+    let a = 5;
+    let b = 10;
+
+    return(
+        <main>
+            <Heading />
+            <p>This sum of a and b is {a+b}.</p>
+        </main>
+    
+    );
+
+  }
