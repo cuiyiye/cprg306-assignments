f7fb2c6923f54cdbd3b0356c8adc668e22d610d7
cuiyiye <130084009+cuiyiye@users.noreply.github.com>
2024-01-25 13:21:09 -0700

    Item added

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,10 +1,10 @@
 export default function Item(props)
 {
     return (
-        <div className = "ml-4">
-            <h2 className = " text-black-900">{props.name}</h2>
-            <p className = "mt-1 text-sm text-gray-500">{quantity}</p>
-            <p className = "mt-1 text-sm text-gray-500">{category}</p>
+        <div className = "m-5 ml-4">
+            <h2 className = " text-black-900 font-bold ">{props.name}</h2>
+            <p className = "mt-1 text-sm text-black-500"> buy {props.quantity} in {props.category} </p>
+        
         </div>
     );
 };  
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -3,8 +3,8 @@ import ItemList from './item-list.js';
 export default function Page() {
   
     return(
-        <main className = "m-4 ml-6">
-            <h1 className = "text-4xl front-bold">Shopping List</h1>
+        <main className = "m-1 ml-1">
+            <h1 className = "text-4xl font-bold">Shopping List</h1>
             <ItemList></ItemList>
         </main>
     
