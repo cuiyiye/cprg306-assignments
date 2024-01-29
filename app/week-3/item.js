aaaa272ff2747c39dbe4a6d5de431b75306fe9fd
cuiyiye <130084009+cuiyiye@users.noreply.github.com>
2024-01-29 12:55:23 -0700

    week-3 finished

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,9 +1,9 @@
 export default function Item(props)
 {
     return (
-        <div className = "m-5 ml-4">
-            <h2 className = " text-black-900 font-bold ">{props.name}</h2>
-            <p className = "mt-1 text-sm text-black-500"> buy {props.quantity} in {props.category} </p>
+        <div className = "m-5 ml-4 bg-gray-900 max-w-xs">
+            <h2 className = " font-bold ">{props.name}</h2>
+            <p className = "mt-1 text-sm"> buy {props.quantity} in {props.category} </p>
         
         </div>
     );
