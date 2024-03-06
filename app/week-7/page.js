2679bfa3c7d1334ebf898ec8567e128a7e8b70df
cuiyiye <130084009+cuiyiye@users.noreply.github.com>
2024-03-05 17:05:36 -0700

    finished

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -18,12 +18,13 @@ export default function Page() {
 
     const handleItemSelect = (item) => {
         const cleanedName = item.name.replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '').trim();
-        /*const regex= cleanedName.replace(/[^a-zA-Z\s]+$/g, '');*/
         const noSpecialChars = cleanedName.replace(/[^a-zA-Z\s]/g, '').trim();
         const units = ['kg', 'pack', 'g','dozen', 'L'];
         const finalName = units.reduce((name, unit) => name.replace(new RegExp(`\\b${unit}\\b`, 'gi'), ''), noSpecialChars).trim();
         setSelectedItemName(cleanedName);
         setSelectedItemName(finalName);
+
+
     };
   
     return(
